0b70dd6c464702d9c15842315ab1cd1eb37b9e89
mohinirathore29 <mohinirathore404@gmail.com>
2023-12-24 13:55:21 +0530

    add the delete functionality of a  Node

--- printLL.cpp
+++ printLL.cpp
@@ -9,6 +9,13 @@ class Node {
         this->data= data;
         this->next = next;
     }
+    ~Node() {
+        cout<<"deleting the Node "<<this->data<<endl;
+         if (next != nullptr) {
+       delete next;
+       next = nullptr;
+   }
+    }
 };
 void insertAtHead(Node* &head,Node* &tail,int data){
     if(head == NULL){
@@ -21,6 +28,15 @@ void insertAtHead(Node* &head,Node* &tail,int data){
     newNode->next = head;
     head =newNode;
     
+}
+int findLength(Node* &head) {
+  int len=0;
+  Node* temp = head;
+  while(temp!=NULL){
+    temp=temp->next;
+    len++;
+  }
+  return len;
 }
 void printLL(Node* &head){
     Node *temp = head;
@@ -40,6 +56,58 @@ void insertAtTail(Node* &head,Node* &tail,int data){
      tail->next =newNode;
      newNode->next =NULL;
 }
+void deleteNodeFromMiddle(Node* &head, Node* &tail,int position) {
+  if(head == NULL) {
+    cout<<"List is empty"<<endl;
+  }
+  //int length =findLength(head);
+  int i=1;
+  Node *prev = head;
+  while(i<position-1){
+    prev =prev->next;
+    i++;
+  }
+  Node *curr =prev->next;
+  prev->next =curr->next;
+  curr->next =NULL;
+  delete curr;
+  return;
+}
+
+void deleteNodeFromHead(Node* &head, Node* &tail) {
+  if(head == NULL) {
+    cout<<"List is empty"<<endl;
+    return;
+  }
+  Node* temp = head;
+  head =head->next;
+  temp->next =NULL;
+  delete temp;
+  }
+  void deleteNodeFromTail(Node* &head, Node* &tail,int position) {
+  if(head == NULL) {
+    cout<<"List is empty"<<endl;
+    return;
+  }
+  int len =findLength(head);
+  //delete last node
+  if(position ==len){
+    int i=1;
+    Node* prev = head;
+    while(i<position-1)
+      {
+        prev = prev->next;
+        i++;
+      }
+    prev->next =NULL;
+    Node *temp =tail;
+    tail =prev;
+    delete temp;
+    return;
+    
+  }
+  
+}
 
 int main() {
     Node* head =NULL;
@@ -50,5 +118,17 @@ int main() {
     insertAtHead(head,tail,80);
     insertAtTail(head,tail,103);
     printLL(head);
-        return 0;
+    cout<<"length"<<endl;
+    int length =findLength(head);
+    cout<<length<<endl;
+    //deleteNodeFromHead(head,tail);
+    printLL(head);
+    cout<<endl;
+    //delete head;
+    //deleteNodeFromTail(head,tail,3);
+    printLL(head);
+    cout<<"delete from middle"<<endl;
+    deleteNodeFromMiddle(head,tail,3);
+    printLL(head);
+    return 0;
 }
